f0311f9034bf96fd6c7ef89ada28724bc18d1514
mathewmarcus <mathewmarcus456@gmail.com>
2018-01-31 22:36:59 -0600

    added test for single route with multiple view functions

--- test/integration/test_single_route.py
+++ test/integration/test_single_route.py
@@ -164,15 +164,66 @@ def test_single_route_docstring_path_ops(app, spec):
     assert '/gists/{gist_id}' in spec._paths
     assert 'get' in spec._paths['/gists/{gist_id}']
     assert 'delete' in spec._paths['/gists/{gist_id}']
-    assert spec._paths['/gists/{gist_id}'] == {
-        'get': {
-            'responses': {
-                200: {
-                    'schema': {
-                        '$ref': '#/definitions/Gist'
-                    }
+    assert spec._paths['/gists/{gist_id}']['get'] == {
+        'responses': {
+            200: {
+                'schema': {
+                    '$ref': '#/definitions/Gist'
                 }
             }
-        },
-        'delete': {}
+        }
+    }
+    assert spec._paths['/gists/{gist_id}']['delete'] == {}
+
+
+
+def test_single_route_multiple_view_functions(app, spec):
+    @app.route('/gists/{gist_id}', methods=['GET'])
+    def gist_detail(gist_id):
+        '''
+        ---
+        get:
+            responses:
+                200:
+                    schema:
+                        $ref: '#/definitions/Gist'
+        '''
+        pass
+
+    @app.route('/gists/{gist_id}', methods=['PUT'])
+    def other_gist_detail(gist_id):
+        '''
+        ---
+        put:
+            responses:
+                204:
+                    schema:
+                        $ref: '#/definitions/Gist'
+        '''
+        pass
+
+
+    spec.add_path(app=app, view=gist_detail)
+    spec.add_path(app=app, view=other_gist_detail)
+
+    assert '/gists/{gist_id}' in spec._paths
+    assert 'get' in spec._paths['/gists/{gist_id}']
+    assert 'put' in spec._paths['/gists/{gist_id}']
+    assert spec._paths['/gists/{gist_id}']['get'] == {
+        'responses': {
+            200: {
+                'schema': {
+                    '$ref': '#/definitions/Gist'
+                }
+            }
+        }
+    }
+    assert spec._paths['/gists/{gist_id}']['put'] == {
+        'responses': {
+            204: {
+                'schema': {
+                    '$ref': '#/definitions/Gist'
+                }
+            }
+        }
     }
