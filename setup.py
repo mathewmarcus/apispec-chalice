21d1cd14238718907b7c4273bc84c0b7740cd874
mathewmarcus <mathewmarcus456@gmail.com>
2018-02-02 21:05:21 -0600

    added README as long_description to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ import os.path
 
 setup(
     name="apispec-chalice",
-    version="0.2.1",
+    version="0.2.2",
     packages=['apispec_chalice'],
     description='Chalice plugin for the apispec library',
     author='Mathew Marcus',
     author_email='mathewmarcus456@gmail.com',
+    long_description=open('README.rst').read(),
     install_requires=['apispec>=0.29.0']
 )
